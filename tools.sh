a06390d16d46fe4d3985e30f4c94fa2e2f9ae25a
Richmond Wang <richmond.wang@quoine.com>
2019-04-05 12:18:56 +0900

    foo

---
+++ tools.sh
@@ -0,0 +1,4 @@
+
+# url encode decode, taken from https://unix.stackexchange.com/a/159254
+alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
